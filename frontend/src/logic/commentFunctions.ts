88d85582fe6c4b12075d4280e68aea63dbc91adf
Yujin Cho <yujincho2002@gmail.com>
2025-05-18 18:08:32 -0700

    attempt at nonworking functional commnets

--- frontend/src/logic/commentFunctions.ts
+++ frontend/src/logic/commentFunctions.ts
@@ -1,20 +1,68 @@
-export function createComment(articleID: string, comment: string, userInfo: any) {
-    // Don't put any empty comments in database
-    if (comment == "" ) {
-        console.log("Empty comment")
-        document.getElementById("error-text")!.style.visibility = "visible";
-        return
-    }
-    document.getElementById("error-text")!.style.visibility = "hidden";
+export async function createComment(
+  articleID: string,
+  comment: string,
+  userInfo: any
+) {
+  handleEmptyComments(comment);
 
-    // If article is not already in the database, put article in database
-    console.log("Article ID:", articleID)
+  // If article is not already in the database, put article in database
+//   console.log("Article ID:", articleID);
+//   await postArticle(articleID);
 
-    // Put comment in database
-    console.log("Username:", userInfo.user["name"])
-    console.log("Comment:", comment)
+  // Put comment in database
+  console.log("Username:", userInfo.user["name"]);
+  console.log("User email:", userInfo.user["email"]);
+  console.log("Comment:", comment);
+  await postComment(articleID, userInfo, comment);
 }
 
-export function deleteComment() {
+async function postComment(articleID: string, userInfo: any, comment: string) {
+  const data = {
+    "article_id": articleID,
+    "username": userInfo.user["name"],
+    "comment": comment,
+  };
 
+  try {
+    await fetch("http://localhost:8000/comments", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8'
+      },
+      body: JSON.stringify(data),
+    });
+  } catch (error) {
+    console.error("Failed to push comment to database", error);
+  }
+  return null;
+}
+
+async function postArticle(articleID: string) {
+  const data = {
+    id: articleID,
+  };
+
+  try {
+    await fetch("http://localhost:8000/articles", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+  } catch (error) {
+    console.error("Failed to push article to database", error);
+  }
+  return null;
+}
+
+export function deleteComment() {}
+
+function handleEmptyComments(comment: string) {
+  if (comment == "") {
+    console.log("Empty comment");
+    document.getElementById("error-text")!.style.visibility = "visible";
+    return;
+  }
+  document.getElementById("error-text")!.style.visibility = "hidden";
 }
